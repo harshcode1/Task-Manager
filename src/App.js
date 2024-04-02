4c13be23c62465653f8735408ca9d5b099af4dd2
Harsh Soni <harsh21csu559@ncuindia.edu>
2024-04-02 23:15:49 +0530

    Chakra UI Initiated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import './App.css';
 import Display from './components/Display';
 import Navbar from './components/Navbar';
+import {
+  ChakraProvider,
+  ChakraBaseProvider,
+  extendBaseTheme,
+  theme as chakraTheme,
+} from '@chakra-ui/react'
+
+
+const { Button } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Button,
+  },
+})
+
 
 
 // => Partially completed will be completed after 3 4 days
 // => Frontend is left Backend Donee...
 function App() {
   return (
-    <>
-    <Navbar/>
-    <Display/>
-    </>
+    <ChakraProvider>
+    <ChakraBaseProvider theme={theme}>
+      <Navbar />
+    </ChakraBaseProvider>
+    </ChakraProvider>
   );
 }
 
-export default App;
+export default App; 
