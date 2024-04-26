5d905e5499dc8b62de30ff47bc9b4fe2f35c27bb
Harsh Soni <harsh21csu559@ncuindia.edu>
2024-04-26 20:41:17 +0530

    fetch API

--- src/components/ButtonGroup.js
+++ src/components/ButtonGroup.js
@@ -2,17 +2,17 @@ import React from "react";
 
 const ButtonGroup = () => {
   return (
-    <div className="d-grid gap-2 col-6 mx-auto mt-5"> 
+    <div className="d-flex justify-content-center gap-2 mt-5"> 
       <button className="btn btn-outline-warning btn-lg" type="button">Add a Task</button>
-      <button className="btn btn-outline-warning btn-lg mt-2" type="button">Show COMPLETED TASKS</button>
-      <div className="dropdown mt-2">
-        <button className="btn btn-outline-warning btn-lg dropdown-toggle w-100" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+      <button className="btn btn-outline-warning btn-lg" type="button">Show COMPLETED TASKS</button>
+      <div className="dropdown">
+        <button className="btn btn-outline-warning btn-lg dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
           Sort NOTES BY
         </button>
-        <ul className="dropdown-menu w-100" aria-labelledby="dropdownMenuButton">
-          <li><a className="dropdown-item" to ="/">Personal</a></li>
-          <li><a className="dropdown-item" to ="/">Professional</a></li>
-          <li><a className="dropdown-item" to ="/">Option 3</a></li>
+        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li><a className="dropdown-item" href="/">Personal</a></li>
+          <li><a className="dropdown-item" href="/">Professional</a></li>
+          <li><a className="dropdown-item" href="/">Option 3</a></li>
         </ul>
       </div>
     </div>
---
+++ src/tasks/context/taskState.js
@@ -0,0 +1,16 @@
+import React,{useState} from React;
+import TaskContext from "./TaskContext";
+
+//=> let's Make Fetch Calls
+
+const TaskCalls = () => {
+
+    const host = 'http://localhost:9000'
+
+
+    //--> GET NOTES
+    const getTask = aync () => {
+        const res = await fetch
+    }
+    
+}
